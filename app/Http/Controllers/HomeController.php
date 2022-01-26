07fa28b51f8bd5f4b1fe3440543264a23db4e1c3
Dario0122 <dario.di.cuia@gmail.com>
2022-01-26 15:29:46 +0100

    population DB

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $comics = Comic::all();
+        return view('home', compact('comics'));
+    }
+}
